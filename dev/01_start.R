3c5d7ab45f1a9a235fdfcfb67fcfe37d08633b2e
Ronan Harrington <ronan_harr@protonmail.com>
2022-05-30 23:49:24 +0100

    Initial set up
    Ran dev\01_start.R as far as licence creation

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,13 @@
 ##
 golem::fill_desc(
   pkg_name = "gutenbergMineR", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional)
+  pkg_title = "Shiny App For Text Mining Project Gutenberg Documents", # The Title of the package containing the App
+  pkg_description = "This Shiny application performs text mining on documents
+  from Project Gutenberg using 'tidytext' and 'gutenbergr'.", # The Description of the package containing the App
+  author_first_name = "Ronan", # Your First Name
+  author_last_name = "Harrington", # Your Last Name
+  author_email = "ronan_harr@protonmail.com", # Your Email
+  repo_url = "https://github.com/rnnh/gutenbergMineR" # The URL of the GitHub Repo (optional)
 )
 
 ## Set {golem} options ----
@@ -33,7 +34,7 @@ golem::set_golem_options()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_gpl3_license() # You can set another license here
 usethis::use_readme_rmd(open = FALSE)
 # Note that `contact` is required since usethis version 2.1.5
 # If your {usethis} version is older, you can remove that param
